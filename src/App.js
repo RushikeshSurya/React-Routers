c4a054f35d83a96f6ba83486538b42bb42c5e63e
Rushikesh.S <Rushikesh.S@Harbingergroup.com>
2023-12-21 19:42:40 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
+
+const routesDefinition = createRoutesFromElements(
+  
+    <Route
+      path="/"
+      element={<RootLayout />}
+      errorElement={<ErrorPage />}
+      children={[
+        <Route path="/" element={<Home />}></Route>,
+        <Route path="/products" element={<Products />}></Route>,
+      ]}
+    />
+  
+);
+const router = createBrowserRouter(routesDefinition);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/MainNavigation";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <h1>You Suck! LOOSER!!!</h1>
+        <h3>try Bullshiting someone else Asshole!</h3>
+      </main>
+    </>
+  );
+}
